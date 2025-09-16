ab9788473cf808eb96c6e93aa3ae16e0c003ecdf
anagheorghiu23 <ghan23zm@student.ju.se>
2025-09-16 16:33:15 +0200

    testing

--- assignment_1_calculator/test/test_unit.py
+++ assignment_1_calculator/test/test_unit.py
@@ -1,7 +1,13 @@
 from assignment_1_calculator.BE.calculator_helper import CalculatorHelper
+# from assertpy import assert_that
 
 class TestCalculator():
-    def test_first(self):
+    def test_add(self):
         calculator = CalculatorHelper()
         value = calculator.add(1,1)
         assert value == 2 
+    
+    def test_subtract(self):
+        calculator = CalculatorHelper()
+        value = calculator.subtract(1,1)
+        assert value == 0 
